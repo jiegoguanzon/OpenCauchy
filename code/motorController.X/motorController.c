b7b90be6129372ab9c94d3f1030b6b8018a6c7c9
Jiego Guanzon <jboguanzon@gmail.com>
2019-09-27 17:01:24 +0800

    FOC-ish.

--- code/motorController.X/motorController.c
+++ code/motorController.X/motorController.c
@@ -33,16 +33,77 @@
 #define INLC    _LATB0
 
 uint8_t timer;
+uint8_t state;
+
+uint16_t time_elapsed = 0;
+uint16_t time_limit = 500;
+uint8_t time_limit_min = 50;
+uint8_t time_elapsed_delta_v = 0;
+uint8_t time_limit_delta_v = 200;
+
+uint8_t handoff = 0;
 
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt (void) {
     _T1IE = 0;
     _T1IF = 0;
     
-    timer++;
-    
-    if (timer >= 1){
-        _LATB15 = !_RB15;
-        timer = 0;
+    time_elapsed++;
+    time_elapsed_delta_v++;
+
+    if (time_elapsed_delta_v >= time_limit_delta_v) {
+        if (--time_limit <= time_limit_min) {
+            time_limit = time_limit_min;
+            //handoff = 1;
+        }
+        time_elapsed_delta_v = 0;
+    }
+
+    if (handoff) {
+
+        timer++;
+        
+        if (timer >= time_limit_min / 4){
+
+            switch (state) {
+
+                case 1:
+                    INLA = !_RB4;
+                    INLB = !_RB2;
+                    INLC = 0;
+                    break;
+                case 2:
+                    INLA = !_RB4;
+                    INLB = 0;
+                    INLC = !_RB0;
+                    break;
+                case 3:
+                    INLA = 0;
+                    INLB = !_RB2;
+                    INLC = !_RB0;
+                    break;
+                case 4:
+                    INLA = !_RB4;
+                    INLB = !_RB2;
+                    INLC = 0;
+                    break;
+                case 5:
+                    INLA = !_RB4;
+                    INLB = 0;
+                    INLC = !_RB0;
+                    break;
+                case 6:
+                    INLA = 0;
+                    INLB = !_RB2;
+                    INLC = !_RB0;
+                    break;
+
+            }
+
+        //_LATB15 = !_RB15;
+
+            timer = 0;
+        }
+
     }
     
     _T1IE = 1;
@@ -50,20 +111,26 @@ void __attribute__((interrupt, no_auto_psv)) _T1Interrupt (void) {
 }
 
 void T1_init(){
+
     T1CON = 0x8000;
     _T1IF = 0;
     _T1IP = 6;
-    PR1 = 80;
+
+    PR1 = 200;
+
+    _TRISB15 = 0;
     _LATB15 = 0;
     timer = 0;
+
     _T1IE = 1;
+
     return;
 }
 
 int main (void) {
 
     TRISA = 0xFFFF;
-    TRISB = 0xFFFF;
+    TRISB = 0xFFC0;
     AD1PCFG = 0xFFFF;
 
     uint16_t data = 0;
@@ -71,27 +138,16 @@ int main (void) {
     char str[16];
 
     LCD_init();
-
-    _TRISB0 = 0;
-    _TRISB1 = 0;
-    _TRISB2 = 0;
-    _TRISB3 = 0;
-    _TRISB4 = 0; 
-    _TRISB15 = 0;
-    T1_init();
-
     spi_init();
 
     INHA = INHB = INHC = 0;
     INLA = INLB = INLC = 0;
 
-    INLC = 1;
-
-    spi_change_hs_peak_source_gate_current(11);
-    spi_change_hs_peak_sink_gate_current(11);
-    spi_change_ls_peak_source_gate_current(11);
-    spi_change_ls_peak_sink_gate_current(11);
-    spi_change_pwm_mode(2);
+    spi_change_hs_peak_source_gate_current(3);
+    spi_change_hs_peak_sink_gate_current(3);
+    spi_change_ls_peak_source_gate_current(3);
+    spi_change_ls_peak_sink_gate_current(3);
+    spi_change_pwm_mode(1);
 
     frame = spi_generate_frame(SPI_READ, 0x2, 0x0);
     data = spi_transfer(frame);
@@ -102,60 +158,87 @@ int main (void) {
     sprintf(str, "Data: 0x%04x", data);
     LCD_send_string(str);
 
+    LCD_cursor_third_line();
+    LCD_send_string("Starting motor.");
+    __delay_ms(1000);
+    LCD_send_character('.');
+    __delay_ms(1000);
+    LCD_send_character('.');
+    __delay_ms(1000);
     LCD_cursor_fourth_line();
-    LCD_send_string("Starting motor...");
-    __delay_ms(2000);
-    INLA = INHB = INLB = 1;
-    __delay_ms(500);
+    LCD_send_string("Motor started.");
 
-    uint8_t state = 1;
-    
+    state = 1;
+    _TRISB15 = 0;
+    _LATB15 = 0;
+    T1_init();
+
     while (1) {
 
-        switch (state) {
+        if (time_elapsed >= time_limit) {
+            time_elapsed = 0;
 
-            case 1:
-                INLA = 1;
-                INHB = 1;
-                INLB = 0;
-                break;
-            case 2:
-                INLA = 1;
-                INHB = 0;
-                INLB = 0;
-                break;
-            case 3:
-                INLA = 1;
-                INHB = 0;
-                INLB = 1;
-                break;
-            case 4:
-                INLA = 0;
-                INHB = 0;
-                INLB = 1;
-                break;
-            case 5:
-                INLA = 0;
-                INHB = 1;
-                INLB = 1;
-                break;
-            case 6:
-                INLA = 0;
-                INHB = 1;
-                INLB = 0;
-                break;
-                
+            switch (state) {
+
+                case 1:
+                    INLA = 1;
+                    INHA = 1;
+                    INLB = 1;
+                    INHB = 0;
+                    INLC = 0;
+                    INHC = 0;
+                    break;
+                case 2:
+                    INLA = 1;
+                    INHA = 1;
+                    INLB = 0;
+                    INHB = 0;
+                    INLC = 1;
+                    INHC = 0;
+                    break;
+                case 3:
+                    INLA = 0;
+                    INHA = 0;
+                    INLB = 1;
+                    INHB = 1;
+                    INLC = 1;
+                    INHC = 0;
+                    break;
+                case 4:
+                    INLA = 1;
+                    INHA = 0;
+                    INLB = 1;
+                    INHB = 1;
+                    INLC = 0;
+                    INHC = 0;
+                    break;
+                case 5:
+                    INLA = 1;
+                    INHA = 0;
+                    INLB = 0;
+                    INHB = 0;
+                    INLC = 1;
+                    INHC = 1;
+                    break;
+                case 6:
+                    INLA = 0;
+                    INHA = 0;
+                    INLB = 1;
+                    INHB = 0;
+                    INLC = 1;
+                    INHC = 1;
+                    break;
+                    
+            }
+
+            if (++state == 7)
+                state = 1;
+
+            //_LATB15 = !_RB15;
+
         }
 
-        if (++state == 7)
-            state = 1;
-
-        //__delay_us(600);
-        __delay_ms(5);
-
     }
-
-    while (1);
 
     return 0;
 
